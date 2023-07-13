5fe4612b70860d6f8aa350c078cd0395df6e360b
gifplane <itact@hotmail.com>
2023-07-14 07:58:25 +0800

    fill triangle

--- TinyRasterization/main.cpp
+++ TinyRasterization/main.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+﻿#include <vector>
 #include <cmath>
 #include "tgaimage.h"
 #include "model.h"
@@ -61,9 +61,41 @@ void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
 
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
 {
-    line(t0, t1, image, color);
-    line(t1, t2, image, color);
-    line(t2, t0, image, color);
+    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+
+    int total_height = t2.y - t0.y;
+    for(int y=t0.y; y<t1.y; ++y)
+    {
+        int segment_height = t1.y - t0.y;
+        float alpha = (float)(y - t0.y) / total_height;
+        float beta = (float)(y - t0.y) / segment_height;
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t0 + (t1 - t0) * beta;
+        if (A.x > B.x) std::swap(A, B);
+        //line(A, B, image, color);
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+        }
+    }
+    for(int y=t1.y;y<t2.y;++y)
+    {
+        int segment_height = t2.y - t1.y + 1;
+        float alpha = (float)(y - t0.y) / total_height;
+        float beta = (float)(y - t1.y) / segment_height; // be careful with divisions by zero 
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t1 + (t2 - t1) * beta;
+        if (A.x > B.x) std::swap(A, B);
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+        }
+    }
+
+    //line(t0, t1, image, green);
+    //line(t1, t2, image, green);
+    //line(t2, t0, image, red);
 }
 
 int main(int argc, char** argv) {
